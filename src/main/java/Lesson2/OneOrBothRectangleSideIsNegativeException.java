da98932030b4c553199c201e92ae9403eb123f44
Dmitriy <dstarshev@gmail.com>
2021-07-18 13:55:09 +0300

    Прошла сторая лекция

---
+++ src/main/java/Lesson2/OneOrBothRectangleSideIsNegativeException.java
@@ -0,0 +1,7 @@
+package Lesson2;
+
+public class OneOrBothRectangleSideIsNegativeException extends RuntimeException{
+    public OneOrBothRectangleSideIsNegativeException(String message) {
+        super(message);
+    }
+}
