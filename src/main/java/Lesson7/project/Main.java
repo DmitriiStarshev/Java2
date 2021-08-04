fc206851305a53abde16ea9458bf74670be56b2c
Дмитрий Старшев <dstarshev@gmail.com>
2021-08-04 23:21:06 +0300

    Прошла 7 лекция

---
+++ src/main/java/Lesson7/project/Main.java
@@ -0,0 +1,8 @@
+package Lesson7.project;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView= new UserInterfaceView();
+        userInterfaceView.runInterface();
+    }
+}
